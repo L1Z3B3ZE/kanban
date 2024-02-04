bdbc54f51bd575fb6fa6ccd92d7c523eb75d66ea
L1Z3B3ZE <mr.lisa.lisa.lisa@mail.ru>
2024-02-05 00:31:45 +0700

    Реализация добавления карточки

--- js/main.js
+++ js/main.js
@@ -1,4 +1,30 @@
 new Vue({
     el: '#app',
+    data: {
+        column1: [],
+        column2: [],
+        column3: [],
+        column4: [],
+        newTaskTitle: '',
+        newTaskDesc: '',
+        deadline: '',
+    },
+    methods: {
+        addTask() {
+            const newTask = {
+                id: Date.now(),
+                title: this.newTaskTitle,
+                description: this.newTaskDesc,
+                deadline: this.deadline,
+                timeout: this.timeout,
+                edited: false
+            }
+            this.column1.push(newTask);
+            this.newTaskDesc = '';
+            this.newTaskTitle = '';
+            this.deadline = '';
+            newTask.creationDate = new Date().toLocaleString();
+        },
+    }
 })
 
