bc8103de43dbb8d1ef9159430c3104dd32e4a0a9
L1Z3B3ZE <mr.lisa.lisa.lisa@mail.ru>
2024-02-06 15:31:31 +0700

    индивидуальное задание

--- js/main.js
+++ js/main.js
@@ -9,12 +9,22 @@ new Vue({
         newTaskDesc: '',
         deadline: '',
         edited: '',
+        editedTitle: '',
         editedDescription:'',
         editedDeadline: '',
         reasonForReturn: ''
     },
     methods: {
         addTask() {
+            const isTaskExistsIn1 = this.column1.some(card => card.title === this.newTaskTitle);
+            const isTaskExistsIn2 = this.column2.some(card => card.title === this.newTaskTitle);
+            const isTaskExistsIn3 = this.column3.some(card => card.title === this.newTaskTitle);
+            const isTaskExistsIn4 = this.column4.some(card => card.title === this.newTaskTitle);
+            if (isTaskExistsIn1 || isTaskExistsIn2 || isTaskExistsIn3 || isTaskExistsIn4) {
+                alert('Задача с таким заголовком уже существует!');
+                this.newTaskTitle = '';
+                return;
+            }
             const newTask = {
                 id: Date.now(),
                 title: this.newTaskTitle,
@@ -37,10 +47,12 @@ new Vue({
         editTasks(card) {
             card.edited = true;
         },
-        saveEditedTask(card,newDescription,newDeadline){
+        saveEditedTask(card,newDescription,newDeadline, newTitle){
+            card.title=newTitle
             card.description=newDescription
             card.deadline=newDeadline
             card.editedDate =new Date().toLocaleString()
+            this.editedTitle=''
             this.editedDeadline=''
             this.editedDescription=''
             card.edited = false
@@ -50,10 +62,20 @@ new Vue({
             this.column2.push(card);
         },
         moveToColumn3(card){
+            const isTaskExists = this.column3.some(existingCard => existingCard.title === card.title);
+            if (isTaskExists) {
+                alert('В третьем столбце уже есть задача с таким же названием!');
+                return;
+            }
             this.column2.splice(this.column2.indexOf(card), 1);
             this.column3.push(card);
         },
         moveToColumn4(card){
+            const isTaskExists = this.column4.some(existingCard => existingCard.title === card.title);
+            if (isTaskExists) {
+                alert('В четвертом столбце уже есть задача с таким же названием!');
+                return;
+            }
             this.column3.splice(this.column3.indexOf(card), 1);
             this.column4.push(card);
             let dateNow= new Date().getTime();
